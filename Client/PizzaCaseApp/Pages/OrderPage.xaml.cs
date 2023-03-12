bed5bfd7ca8e5fdaadae2dfae382998604e5cf59
kajatrijsburg <kajatrijsburg@gmail.com>
2023-03-12 17:07:23 +0100

    adds bindings for the drop down menus

--- Client/PizzaCaseApp/Pages/OrderPage.xaml.cs
+++ Client/PizzaCaseApp/Pages/OrderPage.xaml.cs
@@ -1,9 +1,23 @@
+using PizzaCaseApp.Objects;
+
 namespace PizzaCaseApp.Pages;
 
 public partial class OrderPage : ContentPage
 {
-	public OrderPage()
+    private Order CurrentOrder = new();
+	private Pizza CurrentPizza = new();
+
+    public OrderPage()
 	{
 		InitializeComponent();
+		foreach (string pizzaType in PizzaTypes.AllTypes)
+		{
+			pizza_picker.Items.Add(pizzaType);
+		}
+
+		foreach (string toppingTime in ToppingTypes.AllTypes)
+		{
+			topping_picker.Items.Add(toppingTime);
+		}
 	}
 }
