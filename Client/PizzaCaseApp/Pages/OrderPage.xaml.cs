e0a5255f810042c6a849fd89637cf2cc97d20312
kajatrijsburg <kajatrijsburg@gmail.com>
2023-03-12 13:58:48 +0100

    adds a navbar with a page to order and a page for settings

---
+++ Client/PizzaCaseApp/Pages/OrderPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PizzaCaseApp.Pages;
+
+public partial class OrderPage : ContentPage
+{
+	public OrderPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Client/PizzaCaseApp/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PizzaCaseApp.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
