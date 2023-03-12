3260819830b31fca6afbfca3bbd392d9485e8a9a
kajatrijsburg <kajatrijsburg@gmail.com>
2023-03-12 13:33:46 +0100

    proper commit of the inital app without the extranious files that should have been in the .gitignore

---
+++ Client/PizzaCaseApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaCaseApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Client/PizzaCaseApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaCaseApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
