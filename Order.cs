df9fbf2c66053597f1b61fac04881fb529d96b23
kajatrijsburg <kajatrijsburg@gmail.com>
2024-03-11 10:01:34 +0100

    initial export from Astah

---
+++ Order.cs
@@ -0,0 +1,23 @@
+public class Order
+{
+	public string name;
+
+	public string postalCode;
+
+	public string city;
+
+	public string street;
+
+	public string housenumber;
+
+	public List<Pizza> pizzas;
+
+	private DateTime timeSend;
+
+	public void Accept(Visitor Visitor)
+	{
+
+	}
+
+}
+
---
+++ Server.cs
@@ -0,0 +1,17 @@
+public class Server
+{
+	public int port;
+
+	public List<Order> ordersRecieved;
+
+	private List<Socket> sockets;
+
+	private int maxSockets;
+
+	public Socket Listen()
+	{
+		return null;
+	}
+
+}
+
