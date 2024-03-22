5383c92a798da2a45eb2868e42a9c7028a283ab6
kajatrijsburg <kajatrijsburg@gmail.com>
2024-03-22 10:05:30 +0100

    First Implementation of the Visitor Pattern

---
+++ PizzaCase/IAcceptVisitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCase
+{
+    public interface IAcceptVisitor
+    {
+        void Accept(IVisitor visitor);
+    }
+}
