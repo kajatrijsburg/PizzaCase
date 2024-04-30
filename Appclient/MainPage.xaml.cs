31d0a401f3587ba4ecf7117febf7cf69222558a3
alissa de vegt <alissadevegt@gmail.com>
2024-04-30 17:04:24 +0200

    appclient with udp and tcp

--- Appclient/MainPage.xaml.cs
+++ Appclient/MainPage.xaml.cs
@@ -5,47 +5,119 @@ namespace Appclient
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        bool pizzaord = false;
         public SocketTCP Tcp;
         public SocketUDP Udp;
         public byte[] data;
+        public Order order;
+        public Pizza pizza;
 
         public MainPage()
         {
             InitializeComponent();
             Tcp = new SocketTCP();
             Udp = new SocketUDP();
+            order = new Order();
+            order.pizzas = new List<Pizza>();
+            pizza = new Pizza();
+            pizza.extraToppings = new List<string>();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        /// <summary>
+        /// add the pizza to the pizza to list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAddPizza_Clicked(object sender, EventArgs e)
         {
-            count++;
+            pizza.name = pizzaname.Text;
+            pizza.count = int.Parse(pizzacount.Text);
+            order.pizzas.Add(pizza);
+            pizza = new Pizza();
+            pizza.extraToppings = new List<string>();
+            pizzaname.Text = "";
+            pizzacount.Text = "";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-
-            udpset();
         }
-
-        public void tcpset()
+        /// <summary>
+        /// add the toppings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAddtopping_Clicked(object sender, EventArgs e)
         {
-            Tcp.Connect("192.168.2.2");
-            data = Encoding.ASCII.GetBytes("TCP connect");
-            Tcp.Send(data);
-            CounterBtn.Text = Tcp.decodeddata;
-            Tcp.Close();
+            pizza.extraToppings.Add(Topping.Text);
+            Topping.Text = "";
         }
 
-        public void udpset()
+        /// <summary>
+        /// creates the order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnOrder_Clicked(object sender, EventArgs e)
+        {
+            order.name = name.Text;
+            order.postalCode = postalCode.Text;
+            order.city = city.Text;
+            order.street = street.Text;
+            order.housenumber = housenumber.Text;
+
+
+
+            //test
+            string a  = order.name + " " + order.postalCode + " " + order.city + " " + order.street + " " + order.housenumber;
+
+            foreach(Pizza piz in order.pizzas)
+            {
+                a = a + " " + piz.name + " " + piz.count;
+                foreach (string top in piz.extraToppings)
+                {
+                    a = a+ " " + top;
+                }
+            }
+
+            placeholder.Text = a;
+
+            //reset the order later will be done in sendudp or tcp
+            name.Text = "";
+            postalCode.Text = "";
+            city.Text = "";
+            street.Text = "";
+            housenumber.Text = "";
+            order = new Order();
+            order.pizzas = new List<Pizza>();
+            pizza = new Pizza();
+            pizza.extraToppings = new List<string>();
+
+        }
+
+        /// <summary>
+        /// send through udp 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendudp_Clicked(object sender, EventArgs e)
         {
             Udp.Connect("192.168.2.2");
-            data = Encoding.ASCII.GetBytes("UDP connect");
+            data = Encoding.ASCII.GetBytes("the order udp"); //needs to be replaced with the order that will be send
             Udp.Send(data);
-            CounterBtn.Text = Udp.decodeddata;
+            //CounterBtn.Text = Udp.decodeddata;
             Udp.Close();
+        }
+
+        /// <summary>
+        /// send through tcp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendtcp_Clicked(object sender, EventArgs e)
+        {
+            Tcp.Connect("192.168.2.2");
+            data = Encoding.ASCII.GetBytes("the order tcp"); //needs to be replaced with the order that will be send
+            Tcp.Send(data);
+            //CounterBtn.Text = Tcp.decodeddata;
+            Tcp.Close();
         }
     }
 
